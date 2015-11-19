c4ab96438378483ec99b295600487a3102928c58
Fernando <framirez@imaginabit.com>
2015-11-19 19:51:23 +0000

    card more info ideas

---
+++ app/src/main/java/com/imaginabit/yonodesperdicion/activity/MoreInfo.java
@@ -0,0 +1,32 @@
+package com.imaginabit.yonodesperdicion.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
+
+import com.imaginabit.yonodesperdicion.Base;
+import com.imaginabit.yonodesperdicion.R;
+
+public class MoreInfo extends Base {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more_info);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mContext = getApplicationContext();
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+}
