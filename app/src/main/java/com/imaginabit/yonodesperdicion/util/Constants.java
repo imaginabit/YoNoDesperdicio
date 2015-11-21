5f1a1723f1e94e49f9ffd6b605fd29be970f4d05
Fernando <framirez@imaginabit.com>
2015-11-21 19:41:09 +0000

    download idea data form website

--- app/src/main/java/com/imaginabit/yonodesperdicion/util/Constants.java
+++ app/src/main/java/com/imaginabit/yonodesperdicion/util/Constants.java
@@ -6,8 +6,10 @@ package com.imaginabit.yonodesperdicion.util;
 public class Constants {
 
     // URLs
-    public static final String HOME_URL = "http://controlcenter.movesafer.com";
+    public static final String HOME_URL = "http://beta.yonodesperdicio.org/";
     public static final String IDEAS_API_URL = HOME_URL + "/api/ideas/";
     public static final String ADS_API_URL = HOME_URL + "/api/ads/";
+
+    public static final String longline = "--------------------------------------------------------";
 
 }
