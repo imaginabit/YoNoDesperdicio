2057ae07a0787760ba79f152ce90388c2323f69b
Fernando <framirez@imaginabit.com>
2015-12-19 13:04:45 +0000

    Carga Alimentos Compartidos en el perfil del usuario

--- app/src/main/java/com/imaginabit/yonodesperdicion/activities/ProfileActivity.java
+++ app/src/main/java/com/imaginabit/yonodesperdicion/activities/ProfileActivity.java
@@ -1,7 +1,15 @@
 package com.imaginabit.yonodesperdicion.activities;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,18 +18,36 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.imaginabit.yonodesperdicion.AppSession;
 import com.imaginabit.yonodesperdicion.R;
+import com.imaginabit.yonodesperdicion.adapters.AdsAdapter;
 import com.imaginabit.yonodesperdicion.data.UserData;
+import com.imaginabit.yonodesperdicion.models.Ad;
+import com.imaginabit.yonodesperdicion.models.User;
+import com.imaginabit.yonodesperdicion.utils.AdUtils;
+
+import java.util.List;
 
 public class ProfileActivity extends NavigationBaseActivity {
 
-    UserData mUser;
-    TextView userName;
-    TextView location;
-    TextView weight;
-    RatingBar rating;
-    LinearLayout userads;
+    private final String TAG = getClass().getSimpleName();
+
+    private UserData mUser;
+    private TextView userName;
+    private TextView location;
+    private TextView weight;
+    private RatingBar rating;
+    private LinearLayout userads;
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    private List<Ad> mAds;
+
 
 
     @Override
@@ -31,6 +57,17 @@ public class ProfileActivity extends NavigationBaseActivity {
 
         Toolbar toolbar = setSupportedActionBar();
         setDrawerLayout(toolbar);
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_ads);
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new AdsAdapter(context, mAds);
+        recyclerView.setAdapter(adapter);
 
         // Back button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -50,12 +87,10 @@ public class ProfileActivity extends NavigationBaseActivity {
             weight.setText("Entregados " + Integer.toString(mUser.totalQuantity) + " Kg");
 
             rating.setRating(mUser.rating);
-            /*
-            if (mUser.totalQuantity == 0 && mUser.rating == 0){
-
-            }*/
 
             //userads.setVisibility(View.GONE);
+            getAdsFromWeb( (int)mUser.id );
+
         }
 
     }
@@ -63,7 +98,7 @@ public class ProfileActivity extends NavigationBaseActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         //return super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate( R.menu.profile , menu);
+        getMenuInflater().inflate(R.menu.profile, menu);
         return true;
     }
 
@@ -79,4 +114,45 @@ public class ProfileActivity extends NavigationBaseActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private void getAdsFromWeb(final int userId) {
+
+        User u = new User(userId,"","","","",0,0);
+        Log.d(TAG, "get Ads From Web");
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        final Handler handler = new Handler();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            AdUtils.fetchAds(u, new AdUtils.FetchAdsCallback() {
+                @Override
+                public void done(List<Ad> ads, Exception e) {
+                    if (e == null) {
+                        Log.v(TAG, "---Ads get!");
+                        if (ads != null) {
+                            mAds = ads;
+                            adapter = new AdsAdapter(context, mAds);
+                            recyclerView.setAdapter(adapter);
+                            adapter.notifyDataSetChanged();
+                            Log.e(TAG, "ideas : " + mAds.size());
+                        }
+                    } else {
+                        Log.e(TAG, "error al obtener los Anuncios");
+                        e.printStackTrace();
+                        //wait 5 secons to try again
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                getAdsFromWeb(userId);
+                            }
+                        }, 5000);
+                    }
+                }
+            });
+        } else {
+            Toast.makeText(this, "No se pudieron descargar los anuncios, no hay conexion a internet.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 }
--- app/src/main/java/com/imaginabit/yonodesperdicion/utils/AdUtils.java
+++ app/src/main/java/com/imaginabit/yonodesperdicion/utils/AdUtils.java
@@ -22,6 +22,121 @@ import java.util.List;
 public class AdUtils {
     private static final String TAG = "AdUtils";
 
+    public static void fetchAds(final User u, final FetchAdsCallback callback ){
+        final String TAG= AdUtils.TAG + " fetchAds filter user";
+        AsyncTask<Void, Void, Void> fetchAdsTask = new AsyncTask<Void, Void, Void>() {
+            JSONObject jObj = null;
+            public List<Ad> ads = null;
+            private Exception e = null;
+
+            @Override
+            protected Void doInBackground(Void... params) {
+                String json = null;
+                try {
+                    json = Utils.downloadJsonUrl(Constants.USER_ADS_API_URS + u.getUserId());
+                } catch (IOException e) {
+                    Log.e( TAG , "IOExeption " + e.toString());
+                    this.e = e;
+                }
+
+                // try parse the string to a JSON object
+                try {
+                    jObj = new JSONObject(json);
+                } catch (JSONException e) {
+                    Log.e(TAG + " JSON Parser", "Error parsing data " + e.toString());
+                    this.e = e;
+                } catch (Throwable t) {
+                    Log.e(TAG, "Could not parse malformed JSON: \"" + json + "\"");
+                }
+
+                try {
+                    if (jObj.has("ads")) {
+                        Log.v(TAG,"has ads");
+                        JSONArray jsonItems = null;
+                        try {
+                            jsonItems = jObj.getJSONArray("ads");
+                        } catch (JSONException e) {
+                            this.e = e;
+                            //e2.printStackTrace();
+                        }
+//                        Log.d(TAG, jObj.toString() );
+                        Log.d(TAG,"User has Ads " + jsonItems.length());
+                        if (jsonItems.length() > 0) {
+                            //ads = new ArrayList<>();
+                            ResultAds resultAds;
+                            resultAds = createAdList(jsonItems);
+                            ads = resultAds.ads;
+                            if (resultAds.e!=null) this.e = resultAds.e;
+                        }
+
+                    } else Log.d(TAG, "doInBackground: User without ads");
+                } catch (Exception e) {
+                    //e.printStackTrace();
+                    this.e = e;
+                    Log.e(TAG + " JSON Parser", "Error parsing data " + e.toString());
+                }
+                return null;
+            }
+        };
+        TasksUtils.execute(fetchAdsTask);
+    }
+
+    private static class ResultAds {
+        public List<Ad> ads;
+        public Exception e;
+
+        public ResultAds(List<Ad> ads, Exception e) {
+            this.ads = ads;
+            this.e = e;
+        }
+    }
+
+    private static ResultAds createAdList(JSONArray jsonItems){
+        List<Ad> ads = new ArrayList<>();
+        Exception e = null;
+        for (int i = 0; i < jsonItems.length(); i++) {
+            JSONObject jsonItem = null;
+            try {
+                jsonItem = jsonItems.getJSONObject(i);
+            } catch (JSONException e1) {
+                e = e1;
+            }
+
+            // title, id, category, image_url, introduction
+            //long idea_id = jsonItem.optLong("id", 0L);
+            int ad_id = jsonItem.optInt("id", 0);
+            String title = jsonItem.optString("title", "");
+            String category = jsonItem.optString("food_category", "");
+            String image_url = jsonItem.optString("image", "");
+            String body = jsonItem.optString("body", "");
+            int status = jsonItem.optInt("status", 0);
+            int grams = jsonItem.optInt("grams", 0);
+            int user_id = jsonItem.optInt("user_id", 0);
+            String pick_up_date =jsonItem.optString("pick_up_date", "");
+            String zipcode =jsonItem.optString("zipcode", "");
+
+            Log.v(TAG, "add ad " + jsonItem.toString()  );
+            Log.v(TAG, "add Ad id:" + ad_id + " title:" + title + " cat:" + category + " image:" + image_url + " ");
+            Log.v(TAG, "add Ad id:" + ad_id + " intro: " + zipcode.toString());
+
+            try {
+                //status 3 => producto entregado
+                if ( Utils.isNotEmptyOrNull(title) && status != 3 ) {
+                    //Ad(String title, String body, String imageUrl, int weightGrams, Date expiration, int postalCode, Status status, int userId, String userName)
+                    Ad item = new Ad(ad_id,title,body,image_url,grams,pick_up_date,zipcode,status,user_id,"Usuario");
+
+                    ads.add(item);
+                }
+            } catch ( Exception e1 ){
+                //e.printStackTrace();
+                e = e1;
+            }
+            //Log.d(TAG,"added " + ads.size() );
+        }
+        return new ResultAds(ads,e);
+
+    }
+
     public static void fetchAds(final Activity activity, final FetchAdsCallback callback ){
         final String TAG= AdUtils.TAG + " fetchAds";
         AsyncTask<Void, Void, Void> fetchAdsTask = new AsyncTask<Void, Void, Void>() {
@@ -49,7 +164,6 @@ public class AdUtils {
                     Log.e(TAG, "Could not parse malformed JSON: \"" + json + "\"");
                 }
 
-
                 try {
                     if (jObj.has("ads")) {
                         Log.v(TAG,"has ads");
@@ -63,70 +177,19 @@ public class AdUtils {
 //                        Log.d(TAG, jObj.toString() );
                         Log.d(TAG,"has Ads " + jsonItems.length());
                         if (jsonItems.length() > 0) {
-                            ads = new ArrayList<>();
-                            for (int i = 0; i < jsonItems.length(); i++) {
-                                JSONObject jsonItem = null;
-                                try {
-                                    jsonItem = jsonItems.getJSONObject(i);
-                                } catch (JSONException e) {
-                                    this.e = e;
-                                }
-
-                                // Extract properties
-//                                    "id":11,
-//                                    "title":"Lata de tomate",
-//                                    "body": "El tomate triturado puede ser útil para macarrones con un poco de cebolla. Sus ingredientes son 99,5% tomate, sal y acidulante: ácido cítrico. Valor energético 102 kJ/24 kcal. Su vencimiento es en el 2018. Lo comparto porque tengo muchas conservas de mi huerta en la despensa y la etiqueta está rompiéndose. ",
-//                                    "status": 1,
-//                                    "grams":800,
-//                                    "expiration_date":null,
-//                                    "pick_up_date": "2015-12-15",
-//                                    "comments_enabled":1,
-//                                    "image": "/system/ads/images/000/000/011/original/lata-de-tomate.jpg?1446554898",
-//                                    "zipcode":"28012",
-//                                    "city":"madrid",
-//                                    "province":"madrid",
-//                                    "food_category": "conservas",
-//                                    "user_id":10
-
-                                // title, id, category, image_url, introduction
-                                //long idea_id = jsonItem.optLong("id", 0L);
-                                int ad_id = jsonItem.optInt("id", 0);
-                                String title = jsonItem.optString("title", "");
-                                String category = jsonItem.optString("food_category", "");
-                                String image_url = jsonItem.optString("image", "");
-                                String body = jsonItem.optString("body", "");
-                                int status = jsonItem.optInt("status", 0);
-                                int grams = jsonItem.optInt("grams", 0);
-                                int user_id = jsonItem.optInt("user_id", 0);
-                                String pick_up_date =jsonItem.optString("pick_up_date", "");
-                                String zipcode =jsonItem.optString("zipcode", "");
-
-                                Log.v(TAG, "add ad " + jsonItem.toString()  );
-
-                                Log.v(TAG, "add Ad id:" + ad_id + " title:" + title + " cat:" + category + " image:" + image_url + " ");
-                                Log.v(TAG, "add Ad id:" + ad_id + " intro: " + zipcode.toString());
-
-                                try {
-                                    //status 2 => producto entregado
-                                    if ( Utils.isNotEmptyOrNull(title) && status != 3 ) {
-                                        //public Ad(String title, String body, String imageUrl, int weightGrams, Date expiration, int postalCode, Status status, int userId, String userName) {
-                                        Ad item = new Ad(ad_id,title,body,image_url,grams,pick_up_date,zipcode,status,user_id,"Usuario");
-
-                                        ads.add(item);
-                                    }
-                                } catch ( Exception e ){
-                                    e.printStackTrace();
-                                }
-                                Log.d(TAG,"added " + ads.size() );
-                            }
+                            //ads = new ArrayList<>();
+                            ResultAds resultAds;
+                            resultAds = createAdList(jsonItems);
+                            ads = resultAds.ads;
+                            if (resultAds.e!=null) this.e = resultAds.e;
                         }
+
                     }
                 } catch (Exception e) {
                     //e.printStackTrace();
                     this.e = e;
-//                Log.e(TAG + " JSON Parser", "Error parsing data " + e.toString());
+                    Log.e(TAG + " JSON Parser", "Error parsing data " + e.toString());
                 }
-
                 return null;
             }
 
@@ -148,6 +211,7 @@ public class AdUtils {
         };
         TasksUtils.execute(fetchAdsTask);
     }
+
 
 
 
@@ -240,6 +304,9 @@ public class AdUtils {
         };
         TasksUtils.execute(fetchAdTask);
     }
+
+
+
 
 
 
