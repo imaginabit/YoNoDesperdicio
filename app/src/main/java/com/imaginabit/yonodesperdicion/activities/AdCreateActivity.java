07b874515d379c1abae609fefb534b8b6c1f0581
Fernando <framirez@imaginabit.com>
2015-12-22 12:44:10 +0000

    pasado a produccion la url , inicio rama crear nuevo anuncio

--- app/src/main/java/com/imaginabit/yonodesperdicion/activities/AdCreateActivity.java
+++ app/src/main/java/com/imaginabit/yonodesperdicion/activities/AdCreateActivity.java
@@ -1,19 +1,79 @@
 package com.imaginabit.yonodesperdicion.activities;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.media.Image;
+import android.net.Uri;
+
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.Toast;
-
 import com.imaginabit.yonodesperdicion.R;
 
+import java.io.File;
+
+
 public class AdCreateActivity extends NavigationBaseActivity {
+    ImageView image;
+    EditText title;
+    EditText weight;
+    EditText expiration_date;
+
+    protected static final int CAMERA_REQUEST = 0;
+    protected static final int GALLERY_PICTURE = 1;
+    ImageView img_logo;
+    Bitmap bitmap;
+    String selectedImagePath;
+    private Intent pictureActionIntent = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ad_edit);
         setSupportedActionBar(R.drawable.ic_arrow_back_black);
+
+        //curl -H "Content-Type: application/json"  -H "Authorization: TB1T2pDQuGYExhJQ5vYB" -X POST -d
+        // '{"ad": {
+        //          "title":"probando desde api",
+        //          "body":"un alimento muy rico",
+        //          "grams":"120",
+        //          "status":"1",
+        //          "food_category":"bebidas",
+        //          pick_up_date
+        // }}'
+        // -X POST http://localhost:3000/api/ads
+
+        image =  (ImageView) findViewById(R.id.ad_image);
+        image.setVisibility(View.INVISIBLE);
+        title = (EditText) findViewById( R.id.title);
+        weight = (EditText) findViewById( R.id.weight);
+        expiration_date = (EditText) findViewById( R.id.expiration_date);
+        Button button = (Button) findViewById(R.id.delete_ad);
+        button.setVisibility(View.GONE);
+        FrameLayout frameImage = (FrameLayout) findViewById(R.id.frame_image);
+
+        frameImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startDialog();
+            }
+        });
+
     }
 
     @Override
@@ -37,5 +97,146 @@ public class AdCreateActivity extends NavigationBaseActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void startDialog() {
+        AlertDialog.Builder myAlertDialog = new AlertDialog.Builder(context);
+        myAlertDialog.setTitle("Upload Pictures Option");
+        myAlertDialog.setMessage("How do you want to set your picture?");
+
+        myAlertDialog.setPositiveButton("Gallery",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        Intent pictureActionIntent = null;
+
+                        pictureActionIntent = new Intent(
+                                Intent.ACTION_PICK,
+                                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        startActivityForResult(
+                                pictureActionIntent,
+                                GALLERY_PICTURE);
+
+                    }
+                });
+
+        myAlertDialog.setNegativeButton("Camera",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+
+                        Intent intent = new Intent(
+                                MediaStore.ACTION_IMAGE_CAPTURE);
+                        File f = new File(android.os.Environment
+                                .getExternalStorageDirectory(), "temp.jpg");
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT,
+                                Uri.fromFile(f));
+
+                        startActivityForResult(intent,
+                                CAMERA_REQUEST);
+
+                    }
+                });
+        myAlertDialog.show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        bitmap = null;
+        selectedImagePath = null;
+
+        if (resultCode == RESULT_OK && requestCode == CAMERA_REQUEST) {
+
+            File f = new File(Environment.getExternalStorageDirectory()
+                    .toString());
+            for (File temp : f.listFiles()) {
+                if (temp.getName().equals("temp.jpg")) {
+                    f = temp;
+                    break;
+                }
+            }
+
+            if (!f.exists()) {
+
+                Toast.makeText(getBaseContext(),
+
+                        "Error while capturing image", Toast.LENGTH_LONG)
+
+                        .show();
+
+                return;
+
+            }
+
+            try {
+
+                bitmap = BitmapFactory.decodeFile(f.getAbsolutePath());
+
+                bitmap = Bitmap.createScaledBitmap(bitmap, 400, 400, true);
+
+                int rotate = 0;
+                try {
+                    ExifInterface exif = new ExifInterface(f.getAbsolutePath());
+                    int orientation = exif.getAttributeInt(
+                            ExifInterface.TAG_ORIENTATION,
+                            ExifInterface.ORIENTATION_NORMAL);
+
+                    switch (orientation) {
+                        case ExifInterface.ORIENTATION_ROTATE_270:
+                            rotate = 270;
+                            break;
+                        case ExifInterface.ORIENTATION_ROTATE_180:
+                            rotate = 180;
+                            break;
+                        case ExifInterface.ORIENTATION_ROTATE_90:
+                            rotate = 90;
+                            break;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                Matrix matrix = new Matrix();
+                matrix.postRotate(rotate);
+                bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(),
+                        bitmap.getHeight(), matrix, true);
+
+
+                img_logo.setImageBitmap(bitmap);
+                //storeImageTosdCard(bitmap);
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        } else if (resultCode == RESULT_OK && requestCode == GALLERY_PICTURE) {
+            if (data != null) {
+
+                Uri selectedImage = data.getData();
+                String[] filePath = {MediaStore.Images.Media.DATA};
+                Cursor c = getContentResolver().query(selectedImage, filePath,
+                        null, null, null);
+                c.moveToFirst();
+                int columnIndex = c.getColumnIndex(filePath[0]);
+                selectedImagePath = c.getString(columnIndex);
+                c.close();
+
+//                if (selectedImagePath != null) {
+//                    txt_image_path.setText(selectedImagePath);
+//                }
+
+                bitmap = BitmapFactory.decodeFile(selectedImagePath); // load
+                // preview image
+                bitmap = Bitmap.createScaledBitmap(bitmap, 400, 400, false);
+
+
+                img_logo.setImageBitmap(bitmap);
+
+            } else {
+                Toast.makeText(getApplicationContext(), "Cancelled",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+
     }
 }
