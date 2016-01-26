fe9faf9b65353562676134819c94e2edebd521d9
Fernando <framirez@imaginabit.com>
2016-01-26 21:14:50 +0000

    datepicker on pickup date selector

--- app/src/main/java/com/imaginabit/yonodesperdicion/activities/AdCreateActivity.java
+++ app/src/main/java/com/imaginabit/yonodesperdicion/activities/AdCreateActivity.java
@@ -2,6 +2,7 @@ package com.imaginabit.yonodesperdicion.activities;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -24,6 +25,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -53,6 +55,7 @@ import org.json.JSONObject;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -165,10 +168,42 @@ public class AdCreateActivity extends NavigationBaseActivity {
                 //imageEditable.setImageDrawable(ContextCompat.getDrawable(context,R.drawable.brick));
             }
 
-            //borrar anuncio
 
+            //TODO: borrar anuncio
         }
 
+        // show calendar widget to select date
+        expiration_date.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus){
+                    new DatePickerDialog(AdCreateActivity.this, date, myCalendar
+                            .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                            myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+                }
+            }
+        });
+    }
+
+    Calendar myCalendar = Calendar.getInstance();
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            // TODO Auto-generated method stub
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+    };
+
+    private void updateLabel() {
+
+        String myFormat = "dd/MM/yyyy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat);// , Locale.ENGLISH
+
+        expiration_date.setText(sdf.format(myCalendar.getTime()));
     }
 
     @Override
@@ -604,4 +639,5 @@ public class AdCreateActivity extends NavigationBaseActivity {
             e.printStackTrace();
         }
     }
+
 }
