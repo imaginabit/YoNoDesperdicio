95000c88d9bc850c2b4c38c9b9db9c0d5e82bb36
Fernando <framirez@imaginabit.com>
2018-11-03 23:12:29 +0000

    Show simple notification when app is closed

--- app/src/main/java/com/imaginabit/yonodesperdicion/gcm/MyFirebaseMessagingService.java
+++ app/src/main/java/com/imaginabit/yonodesperdicion/gcm/MyFirebaseMessagingService.java
@@ -22,6 +22,7 @@ import com.imaginabit.yonodesperdicion.activities.MainActivity;
 import com.imaginabit.yonodesperdicion.activities.MessagesActivity;
 import com.imaginabit.yonodesperdicion.activities.MessagesChatActivity;
 import com.imaginabit.yonodesperdicion.data.AdsContract;
+import com.imaginabit.yonodesperdicion.helpers.VolleySingleton;
 import com.imaginabit.yonodesperdicion.models.Conversation;
 import com.imaginabit.yonodesperdicion.models.Message;
 import com.imaginabit.yonodesperdicion.utils.MessagesUtils;
@@ -73,6 +74,8 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             String conversation_id = data.get("conversation");
             final String[] notificationText = new String[1];
             String notificationBody = "";
+
+            // descarga las conversaciones...
             if (Utils.isNotEmptyOrNull( conversation_id )){
                 Log.d(TAG, "onMessageReceived: " + "no null");
                 //get data from conversation
@@ -80,58 +83,57 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 Conversation c;
                 c = new Conversation(Integer.parseInt(conversation_id),"c");
                 conversations.add(c);
-                MessagesUtils.getConversationMessagesInbox(conversations, new MessagesUtils.MessagesCallback() {
-                    @Override
-                    public void onFinished(List<Message> messages, Exception e, ArrayList data) {
-                        Log.d(TAG, "MessagesCB onFinished() called with: messages = [" + messages.size() + "], e = [" + e + "], data = [" + data.size() + "]");
-                        if (data.size() > 0) {
+//                Log.d(TAG, "onMessageReceived: calling messageutils.getConversationMessagesInbox: (conversations "+conversations+ "\n\n,data "+data+".\n\n");
 
-                            Log.d(TAG, "onFinished: last messages "+ messages.get(messages.size()-1) );
-                            Conversation cs  = (Conversation) data.get(0);
+                try {
+                    //when the app is closed this cause error but still we can show a basic msg
+                    VolleySingleton.getRequestQueue();
 
-                            Log.d(TAG, "onFinished: conversation 0 : " + cs );
-                            List<Message> ms = (ArrayList<Message>) cs.getMessages();
+                    MessagesUtils.getConversationMessagesInbox(conversations, new MessagesUtils.MessagesCallback() {
+                        @Override
+                        public void onFinished(List<Message> messages, Exception e, ArrayList data) {
+                            Log.d(TAG, "MessagesCB onFinished() called with: messages = [" + messages.size() + "], e = [" + e + "], data = [" + data.size() + "]");
+                            if (data.size() > 0) {
 
-                            Log.d(TAG, "ms size  "+ ms.size() + " body last msg " + ms.get(ms.size()-1).getBody() );
-                            Log.d(TAG, "ms size  msg string "+  ms.get(ms.size()-1).toString() );
-                            Message lastMs = ms.get(ms.size()-1);
-                            //update conversation info
-                            cs.setSubject(lastMs.getSubject());
+                                Log.d(TAG, "onFinished: last messages "+ messages.get(messages.size()-1) );
+                                Conversation cs  = (Conversation) data.get(0);
 
-                            showNotification(lastMs, cs);
+                                Log.d(TAG, "onFinished: conversation 0 : " + cs );
+                                List<Message> ms = (ArrayList<Message>) cs.getMessages();
+
+                                Log.d(TAG, "ms size  "+ ms.size() + " body last msg " + ms.get(ms.size()-1).getBody() );
+                                Log.d(TAG, "ms size  msg string "+  ms.get(ms.size()-1).toString() );
+                                Message lastMs = ms.get(ms.size()-1);
+                                //update conversation info
+                                cs.setSubject(lastMs.getSubject());
+                                Log.d(TAG, "onFinished: showNotification lastMs"+lastMs+", cs "+cs+". ");
+
+                                showNotification(lastMs, cs);
+                            }
                         }
-                    }
 
-                    @Override
-                    public void onFinished(List<Message> messages, Exception e) {
-                        Log.d(TAG, "MessagesCB onFinished() called with: messages  = [" + messages.size() + "], e = [" + e + "]");
-                    }
+                        @Override
+                        public void onFinished(List<Message> messages, Exception e) {
+                            Log.d(TAG, "MessagesCB onFinished() called with: messages  = [" + messages.size() + "], e = [" + e + "]");
+                        }
 
-                    @Override
-                    public void onError(String errorMessage) {
-                        Log.d(TAG, "MessagesCB onError() called with: errorMessage = [" + errorMessage + "]");
-                    }
-                });
+                        @Override
+                        public void onError(String errorMessage) {
+                            Log.d(TAG, "MessagesCB onError() called with: errorMessage = [" + errorMessage + "]");
+                        }
+                    });
+
+                } catch (Exception e){
+                    e.printStackTrace();
+                    showNotificationBasic("Yo No Desperdicio","Tienes un mensaje nuevo",100);
+                }
+
             }
         }
     }
 
-
-    /**
-     * Crea y muestra la notificacion
-     * problems if manifest dont have the property "android:launchMode="singleInstance""
-     * @param ms Mensage info
-     * @param conversation Conversation info
-     */
-    private void showNotification(Message ms, Conversation conversation){
-        Log.d(TAG, "showNotification() called");// with: ms = [" + ms + "], conversation = [" + conversation + "]");
-        String notificationBody = ms.getBody();
-        String title = ms.getSubject();
-        int cID =  conversation.getId();
-
-        Log.d(TAG, "showNotification: title:"+ title+ ", notificationBody:"+ notificationBody+".");
-
-
+    private void showNotificationBasic(String body, String title, int notification_id){
+        Log.d(TAG, "showNotificationBasic() called with: body = [" + body + "], title = [" + title + "]");
 
         NotificationManager mNotificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
@@ -155,23 +157,78 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_stat_brick_notif)
                 .setLargeIcon(largeicon)
-                .setContentTitle(title)
-                .setContentText(notificationBody)
+                .setContentTitle( title )
+                .setContentText( body )
                 .setAutoCancel(true);
 
-//        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext())
-//                .setSmallIcon(R.drawable.ic_stat_brick_notif)
-//                .setLargeIcon(largeicon)
-//                .setContentTitle( title )
-//                .setContentText( notificationBody )
-//                .setAutoCancel(true)
-//                ;
+        //Intent open chat window
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        resultIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+//        // The stack builder object will contain an artificial back stack for the
+//        // started Activity.
+//        // This ensures that navigating backward from the Activity leads out of
+//        // your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addNextIntentWithParentStack(resultIntent);
+
+
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+
+        mNotificationManager.notify(notification_id, mBuilder.build());
+
+    }
+
+
+    /**
+     * Crea y muestra la notificacion
+     * problems if manifest dont have the property "android:launchMode="singleInstance""
+     * @param ms Mensage info
+     * @param conversation Conversation info
+     */
+    private void showNotification(Message ms, Conversation conversation){
+        Log.d(TAG, "showNotification() called");// with: ms = [" + ms + "], conversation = [" + conversation + "]");
+        String body = ms.getBody();
+        String title = ms.getSubject();
+        int cID =  conversation.getId();
+
+        Log.d(TAG, "showNotification: title:"+ title+ ", notificationBody:"+ body+".");
+
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        String CHANNEL_ID = "my_channel_01";
+        if ( Build.VERSION.SDK_INT >= Build.VERSION_CODES.O ){
+            CHANNEL_ID = "channel_ynd_1";
+            CharSequence name = "ynd";
+            String Description = "Yo no desperdicio";
+//            int importance = NotificationManager.IMPORTANCE_HIGH;
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+            mChannel.setDescription(Description);
+            mChannel.enableLights(true);
+            mChannel.setLightColor(Color.BLUE);
+            mChannel.enableVibration(true);
+            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            mChannel.setShowBadge(false);
+            mNotificationManager.createNotificationChannel(mChannel);
+        }
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_stat_brick_notif)
+                .setLargeIcon(largeicon)
+                .setContentTitle( title )
+                .setContentText( body )
+                .setAutoCancel(true);
 
         //Intent open chat window
         Intent resultIntent = new Intent(this, MessagesActivity.class);
         resultIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-
-
 
         // The stack builder object will contain an artificial back stack for the
         // started Activity.
@@ -187,15 +244,14 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                         0,
                         PendingIntent.FLAG_UPDATE_CURRENT
                 );
+
         mBuilder.setContentIntent(resultPendingIntent);
 
+        int timestamp = (int) System.currentTimeMillis();
+        int notification_id = cID+timestamp;
 
 
-        // mId allows you to update the notification later on.
-        // mId == Conversation ID
-        int mId = cID;
-
-        mNotificationManager.notify(mId, mBuilder.build());
+        mNotificationManager.notify(notification_id, mBuilder.build());
     }
 
     /**
@@ -248,4 +304,11 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         mNotificationManager.notify(mId, mBuilder.build());
     }
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+        Log.e(TAG, "onNewToken: s:"+ s+"." );
+    }
+
 }
--- app/src/main/java/com/imaginabit/yonodesperdicion/utils/MessagesUtils.java
+++ app/src/main/java/com/imaginabit/yonodesperdicion/utils/MessagesUtils.java
@@ -5,7 +5,6 @@ import android.app.ProgressDialog;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
-import android.database.Cursor;
 import android.net.Uri;
 import android.util.Log;
 
